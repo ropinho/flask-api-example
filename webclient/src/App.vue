4248f10435a2c6ba888afe71f5e1ff54072aa84a
Ronaldd Pinho <ronaldppinho@gmail.com>
2021-03-31 11:33:46 -0300

    feat: add initial directory structure

---
+++ webclient/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <p>{{ greeting }}</p>
+    <p>{{ flaskGreeting }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: function () {
+    return {
+      greeting: "Hello from Vue",
+      flaskGreeting: ""
+    }
+  },
+  created: async function () {
+    const gResponse = await fetch("http://localhost:5000/greeting");
+    const gObject = await gResponse.json();
+    console.log(gObject);
+    this.flaskGreeting = gObject.msg;
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
