e339a238ca6c966b0caa8a9c11b221a6d1561368
Ronaldd Pinho <ronaldppinho@gmail.com>
2021-04-01 21:13:29 -0300

    feat: add oauth2 login using google-api-client library
    - Using Google API client library
    - No more use Flask-login
    - Using flask.session to store credentials
    - Using JSON file with client credentials locally

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# flask-vue-app
-Uma aplicação web fullsatck exemplo usando Flask e Vue.js
+# Super OrgContact
+
+Uma aplicação web fullsatck exemplo usando Flask e Vue.js para organização de
+contatos.
+Um usuário efetua o login do Google e pode ver seus contatos agrupados pelo
+domínio do email.
+
+
--- api/app.py
+++ api/app.py
@@ -1,52 +1,26 @@
-# Python standard libraries
-import json
 import os
-import sqlite3
-
-# Third-party libraries
-from flask import Flask, redirect, request, url_for, jsonify
-from flask_login import (
-    LoginManager,
-    current_user,
-    login_required,
-    login_user,
-    logout_user,
-)
-from oauthlib.oauth2 import WebApplicationClient
+import flask
 import requests
 
-from db import init_db_command
-from user import User
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import Flow
+from googleapiclient.discovery import build
 
-# Configuration
+# Configurações
 GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
 GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
 GOOGLE_DISCOVERY_URL = (
     "https://accounts.google.com/.well-known/openid-configuration"
 )
+SCOPES = [
+    "openid",
+    "https://www.googleapis.com/auth/userinfo.profile",
+    "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/contacts"
+]
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
-
-# User session management setup
-# https://flask-login.readthedocs.io/en/latest
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-# OAuth 2 client setup
-client = WebApplicationClient(GOOGLE_CLIENT_ID)
-
-# Inicializa o banco de dados
-try:
-    init_db_command()
-except sqlite3.OperationalError:
-    # Assume que ele ja foi criado
-    pass
-
-# Funcao ajuda carregar informacoes de usuarios do banco de dados
-@login_manager.user_loader
-def load_user(user_id):
-    return User.get(user_id)
 
 # Endpoints
 # Homepage:       /
@@ -54,114 +28,86 @@ def load_user(user_id):
 # Login Callback: /login/callback
 # Logout:         /logout
 
-# HOMEPAGE
 #
-# Isso não é nada sofisticado visualmente, mas você adicionará uma lógica
-# interessante para exibir algo diferente se um usuário estiver conectado.
-# Quando ele não estiver conectado, aparecerá um link que diz Login do Google.
-# Pressionar o link os redirecionará para seu endpoint "/login", que iniciará o
-# fluxo de login. Após um login bem-sucedido, a página inicial agora exibirá o
-# e-mail do usuário do Google e sua foto pública do perfil do Google!
+#
 @app.route("/")
 def index():
-    if current_user.is_authenticated:
-        response_data = {
-            "id": current_user.id,
-            "name": current_user.name,
-            "email": current_user.email,
-            "profile_picture": current_user.profile_pic
-        }
-        return jsonify(response_data)
-    else:
+    # Se não há credenciais de sessão, o usuário recebe um link para o
+    # endpoint de login
+    if 'credentials' not in flask.session:
         return '<a class="button" href="/login">Google Login</a>'
+    
+    # Pega as credenciais da sessão do usuário logado e inicia a integração com
+    # o serviço da Google People API
+    credentials = Credentials(**flask.session['credentials'])
+    people_service = build('people', 'v1', credentials=credentials)
 
-# LOGIN 
+    # Obter o nome e email da conta do Google do usuario logado
+    profile = people_service.people().get(
+            resourceName='people/me', personFields='names,emailAddresses'
+    ).execute()
 
-# Função rápida e ingênua para recuperar a configuração do provedor do Google
-def get_google_provider_cfg():
-    return requests.get(GOOGLE_DISCOVERY_URL).json()
+    # Obter a lista de conexões/contatos
+    connections = people_service.people().connections().list(
+            resourceName='people/me', personFields='names,emailAddresses'
+    ).execute()
 
-# O campo do documento de configuração do provedor de que você precisa é chamado
-# de authorization_endpoint. Ele conterá o URL que você precisa usar para
-# iniciar o fluxo OAuth 2 com o Google a partir de seu aplicativo cliente.
+    return flask.jsonify({'userinfo': profile, **connections})
+
+
+# Inicia o fluxo de login usando a biblioteca de cliente oAuth2 do Google
 @app.route("/login")
 def login():
-    # Descubra qual URL deve ser acessado para fazer login no Google
-    google_provider_cfg = get_google_provider_cfg()
-    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+    # Usa as credenciais do arquivo local 'credentials.json'
+    flow = Flow.from_client_secrets_file('credentials.json', scopes=SCOPES)
+    flow.redirect_uri = flask.request.base_url + "/callback"
 
-    # Usa a biblioteca para construir a solicitação de login do Google e
-    # fornecer escopos que permitem recuperar o perfil do usuário do Google
-    request_uri = client.prepare_request_uri(
-            authorization_endpoint,
-            redirect_uri=request.base_url + "/callback",
-            scope=["openid", "email", "profile"]
+    authorization_url, state = flow.authorization_url(
+            access_type='offline',
+            include_granted_scope='true'
     )
-    return redirect(request_uri)
+    flask.session['state'] = state
+     
+    return flask.redirect(authorization_url)
 
-# LOGIN CALLBACK
-#
-# Quando o Google enviar de volta esse código exclusivo, ele o enviará a este
-# ponto de extremidade de retorno de chamada de login em seu aplicativo.
-# Portanto, sua primeira etapa é definir o endpoint e obter esse código.
+
+# Recebe a resposta do servidor de autorização do Google e armazena as
+# credenciais em uma sessão do app Flask
 @app.route("/login/callback")
 def callback():
-    # Pegar o código de autenticação que o Google enviou para esse endpoint
-    code = request.args.get("code")
-
-    # Descubra qual URL atingir para obter tokens que permitem que você pergunte
-    # coisas em nome de um usuário
-    google_provider_cfg = get_google_provider_cfg()
-    token_endpoint = google_provider_cfg["token_endpoint"]
+    state = flask.session['state']
+    flow = Flow.from_client_secrets_file(
+            'credentials.json', scopes=SCOPES, state=state)
+    flow.redirect_uri = flask.url_for('callback', _external=True)
     
-    # Preparar a requisição pra receber os tokens
-    token_url, headers, body = client.prepare_token_request(
-            token_endpoint,
-            authorization_response=request.url,
-            redirect_url=request.base_url,
-            code=code
-    )
+    authorization_response = flask.request.url
+    flow.fetch_token(authorization_response=authorization_response)
 
-    # Faz a requisição POST pra receber os tokens do Google
-    token_response = requests.post(token_url, headers=headers, data=body,
-            auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET))
-
-    # Parse tokens
-    client.parse_request_body_response(json.dumps(token_response.json()))
-
-    # Agora que temos os tokens, podemos fazer uma requisição ao endpoint do
-    # Google que fornece as informações básicas de usuário: userinfo_endpoint.
-    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
-    uri, headers, body = client.add_token(userinfo_endpoint)
-    userinfo_response = requests.get(uri, headers=headers, data=body)
-
-    unique_id = userinfo_response.json()["sub"]
-    user_email = userinfo_response.json()["email"]
-    user_name = userinfo_response.json()["given_name"]
-    picture = userinfo_response.json()["picture"]
-
-    # Cria novo usuário no app com as informações de usuário obtidas do Google
-    user = User(unique_id, user_name, user_email, picture)
-    if not User.get(unique_id):
-        User.create(unique_id, user_name, user_email, picture)
-
-    # Iniciliza sessão de usuário
-    login_user(user)
+    # Armazena as informações de credenciais da sessão atual do usuário 
+    credentials = flow.credentials
+    flask.session['credentials'] = credentials_to_dict(credentials)
 
     # Redireciona o usuario para a homepage
-    return redirect(url_for("index"))
+    return flask.redirect(flask.url_for("index"))
 
-
-# LOGOUT
 
 @app.route("/logout")
-@login_required
 def logout():
-    logout_user()
-    return redirect(url_for("index"))
+    if 'credentials' in flask.session:
+        flask.session.pop('credentials', None)
+    return flask.redirect(flask.url_for('index'))
 
-# Pra usar o OAuth2 é preciso estar se comunicando sobre o HTTPS e não HTTP
-# Pra deixar isso padrão, o aplicativo é executado com certificado "adhoc".
-#if __name__ == "__main__":
-#    app.run(ssl_context='adhoc')
+
+# Converte um objeto de credenciais para uma representação de dicionário
+def credentials_to_dict(credentials):
+    return {'token': credentials.token,
+        'refresh_token': credentials.refresh_token,
+        'token_uri': credentials.token_uri,
+        'client_id': credentials.client_id,
+        'client_secret': credentials.client_secret,
+        'scopes': credentials.scopes}
+
+
+if __name__ == "__main__":
+    app.run('localhost', 5000, debug=True)
 
